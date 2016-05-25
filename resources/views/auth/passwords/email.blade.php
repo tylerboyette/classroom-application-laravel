da15cc19d38d59ac9a124e8acf168c6cfd9618a5
adriantelles <adriantellesweb@gmail.com>
2016-05-25 13:58:53 -0400

    Minor changes to the password reset templates

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,49 @@
-@extends('layouts.app')
+@extends('layouts.app2')
 
-<!-- Main Content -->
+@section('title', 'Email')
+
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+  <div class="row">
+    <div class="col-xs-12 col-md-8 col-md-offset-2">
+      <div class="panel panel-info">
+        <div class="panel-heading">Reset Password</div>
 
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input type="email" class="form-control" name="email" value="{{ old('email') }}">
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-envelope"></i>Send Password Reset Link
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+        <div class="panel-body">
+          @if (session('status'))
+            <div class="alert alert-success">
+              {{ session('status') }}
             </div>
+          @endif
+
+          <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
+            {{ csrf_field() }}
+
+            <!-- Email Address -->
+            <div class="form-group{{ $errors->has('email') ? ' has-error' : ''}}">
+              <label class="col-md-4 control-label">Email Address</label>
+              <div class="col-md-6">
+                <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+
+                @if ($errors->has('email'))
+                  <span class="help-block">
+                    <strong>{{ $errors->first('email') }}</strong>
+                  </span>
+                @endif
+              </div>
+            </div>
+
+            <!-- Send Password Link Button -->
+            <div class="form-group">
+              <div class="col-md-6 col-md-offset-4">
+                <button type="submit" class="btn btn-primary">
+                  <i class="fa fa-btn fa-envelope"></i>Send Password Reset Link
+                </button>
+              </div>
+            </div>
+
+          </form>
         </div>
+      </div>
     </div>
-</div>
+  </div>
 @endsection
