c1e4f2d0df4b4b40a2fe452c33f4c4c0d6189ba7
adriantelles <adriantellesweb@gmail.com>
2016-05-25 11:02:38 -0400

    Created a new template for the home page

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app2')
+
+@section('title', 'Home')
+
+@section('content')
+  <div class="row">
+    <div class="col-xs-12 col-md-10 col-md-offset-1">
+      <div class="well">
+        <p>Some Text Goes Here.</p>
+      </div>
+    </div>
+  </div>
+@endsection
