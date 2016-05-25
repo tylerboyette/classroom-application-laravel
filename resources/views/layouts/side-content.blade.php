3db25d07d523f4edfd563c25f7f0616946c96665
adriantelles <adriantellesweb@gmail.com>
2016-05-25 15:56:08 -0400

    Moved the side content to a separate file so that it's being displayed on all pages

---
+++ resources/views/layouts/side-content.blade.php
@@ -0,0 +1,9 @@
+<div class="well">
+  <h4>Courses</h4>
+  <p>You are not currently taking any courses.</p>
+</div>
+
+<div class="well">
+  <h4>Messages</h4>
+  <p>Currently you have no messages.</p>
+</div>
