efa71ecb574f63e15bf6090e846b89c5a9850f0b
adriantelles <adriantellesweb@gmail.com>
2016-05-25 14:43:11 -0400

    Created different sections for content that will be displayed on the homepage

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,13 +1,27 @@
-@extends('layouts.app2')
+@extends('layouts.app')
 
 @section('title', 'Home')
 
 @section('content')
   <div class="row">
-    <div class="col-xs-12 col-md-10 col-md-offset-1">
+    <div class="col-xs-12 col-md-4">
       <div class="well">
-        <p>Some Text Goes Here.</p>
+        <h4>Courses</h4>
+        <p>You are not currently taking any courses.</p>
+      </div>
+
+      <div class="well">
+        <h4>Messsages</h4>
+        <p>Currently, you have don't have any messages.</p>
       </div>
     </div>
+
+    <div class="col-xs-12 col-md-8">
+      <div class="panel panel-info">
+        <div class="panel-heading">See What's Happening...</div>
+        <div class="panel-body">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer justo velit, lacinia eget feugiat vel, facilisis in sapien. In quis porttitor diam, id dictum libero. Morbi imperdiet nec felis vitae porttitor. Donec bibendum dui vitae neque lobortis, ut maximus ante ornare. Suspendisse pretium dui id nibh iaculis vehicula. Cras sapien elit, condimentum et finibus a, mattis at enim. Vivamus non eros vestibulum, eleifend nunc eget, malesuada enim. Donec arcu lectus, blandit non sagittis id, facilisis nec purus. Vestibulum finibus lobortis elit nec accumsan. Ut ac ullamcorper orci, et malesuada urna. Nullam turpis arcu, gravida id dui ut, aliquet viverra elit. Sed blandit, urna quis fringilla auctor, elit lectus tristique mauris, laoreet faucibus arcu ipsum fermentum tortor. Nulla bibendum, enim ac lacinia viverra, arcu nunc eleifend nisi, eu mattis diam felis et leo. Vestibulum cursus, urna et imperdiet mattis, nisi massa placerat sapien, malesuada tincidunt ex felis gravida nunc. Donec sollicitudin suscipit odio, quis aliquam neque imperdiet vel.</p>
+        </div>
+      </div>
   </div>
 @endsection
