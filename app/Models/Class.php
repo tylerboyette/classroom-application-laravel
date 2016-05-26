04fa399c5d12b015f5abc63cc5d854e87589b962
adriantelles <adriantellesweb@gmail.com>
2016-05-26 16:54:52 -0400

    Created a new model for the Class object

---
+++ app/Models/Class.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Class extends Model
+{
+    /**
+     * The attributes that are mass assignable
+     * 
+     * @var array
+     */
+    protected $fillable = [
+        'subject', 'title', 'course', 'section'
+    ];
+
+    /** 
+     * Capitalizes the subject to keep consistency
+     * 
+     * @param String $value 
+     * @return string
+     */
+    public function setSubjectAttribute($value)
+    {
+        $this->attributes['subject'] = strtoupper($value);
+    }
+
+    /**
+     * Capitalizes the first character of the string uppercase
+     * to keep consistency
+     * 
+     * @param String $value
+     * @return string
+     */
+    public function setTitleAttribute($value) 
+    {
+        $lowercase = strtolower($value);
+        $this->attributes['title'] = ucfirst($lowercase);
+    }
+
+    /**
+     * The users that belong to this class
+     *   
+     */
+    public function users()
+    {
+        return $this->belongsToMany('App\Models\User');
+    }
+}
