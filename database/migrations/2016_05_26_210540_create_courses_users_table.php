6a436deefef3cb129ac468ea79f573f0b0b8efa6
adriantelles <adriantellesweb@gmail.com>
2016-05-26 17:24:50 -0400

    Created a new migration for the pivot table in the many-to-many relationship between users and courses

---
+++ database/migrations/2016_05_26_210540_create_courses_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCoursesUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('course_user', function (Blueprint $table) {
+            $table->integer('user_id');
+            $table->integer('course_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('course_user');
+    }
+}
