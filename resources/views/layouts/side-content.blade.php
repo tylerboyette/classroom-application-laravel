f342999cb690913ba2385af03ca411d734966ff1
adriantelles <adriantellesweb@gmail.com>
2016-05-27 08:50:24 -0400

    Cosmetic changes and showing the list of courses if there are any available

--- resources/views/layouts/side-content.blade.php
+++ resources/views/layouts/side-content.blade.php
@@ -1,9 +1,22 @@
 <div class="well">
   <h4>Courses</h4>
-  @if (Auth::user()->role == 'teacher')
-    <p>Currently, you do not have any classes.</p>
+  @if (Auth::user()->courses()->get())
+    <div class="list-group">
+      @foreach (Auth::user()->courses()->get() as $course)
+        <a href="course/{{ $course->id }}" class="list-group-item list-group-item-info">
+          <h4 class="list-group-item-heading">
+            {{ $course->subject }} {{ $course->course }}-{{ $course->section }}
+          </h4>
+          <p class="list-group-item-text">{{ $course->title }}</p>
+        </a>
+      @endforeach
+    </div>
   @else
-    <p>You are not currently taking any courses.</p>
+    @if (Auth::user()->role == 'teacher')
+      <div class="alert alert-danger" role="alert">You do not have any active courses.</div>
+    @else
+      <div class="alert alert-danger" role="alert">You are no taking any courses.</div>
+    @endif
   @endif
 </div>
 
