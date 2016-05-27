a7efacdcc2b6887aca4eba4311f641eeb0c22731
adriantelles <adriantellesweb@gmail.com>
2016-05-27 16:21:03 -0400

    Minor changes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -26,6 +26,7 @@ class UserController extends Controller
      */
     public function show() 
     {
+        //return Auth::user()->courses()->get();
         return view('pages.user.profile');
     }
 
