d77c176eaa3ff9b79f956caa8140eefe2700158a
adriantelles <adriantellesweb@gmail.com>
2016-05-28 19:28:54 -0400

    Minor changes to the layout

--- resources/views/pages/course/show.blade.php
+++ resources/views/pages/course/show.blade.php
@@ -50,68 +50,66 @@
 
       <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labellledby="headingOne">
         <div class="panel-body">
-          <div class="col-xs-12 col-md-10 col-md-offset-1">
-            <div class="well">
-              <form class="form-horizontal" role="form" method="POST" action="{{ url('course/' . $course->id) }}">
-                {{ csrf_field() }}
-                {{ method_field('PUT') }}
-                <!-- Subject -->
-                <div class="form-group{{ $errors->has('subject') ? ' has-error': '' }}">
-                  <label class="col-md-3 control-label">Subject</label>
-                  <div class="col-md-6">
-                    <input type="text" class="form-control" name="subject" value="{{ old('subject') ? old('subject') : $course->subject }}">
+          <div class="col-xs-12 col-md-12">
+            <form class="form-horizontal" role="form" method="POST" action="{{ url('course/' . $course->id) }}">
+              {{ csrf_field() }}
+              {{ method_field('PUT') }}
+              <!-- Subject -->
+              <div class="form-group{{ $errors->has('subject') ? ' has-error': '' }}">
+                <label class="col-md-3 control-label">Subject</label>
+                <div class="col-md-6">
+                  <input type="text" class="form-control" name="subject" value="{{ old('subject') ? old('subject') : $course->subject }}">
 
-                    @if ($errors->has('subject'))
-                      <span class="help-block"><strong>{{ $errors->first('subject') }}</strong></span>
-                    @endif
-                  </div>
+                  @if ($errors->has('subject'))
+                    <span class="help-block"><strong>{{ $errors->first('subject') }}</strong></span>
+                  @endif
                 </div>
+              </div>
 
-                <!-- Title -->
-                <div class="form-group{{ $errors->has('title') ? ' has-error': '' }}">
-                  <label class="col-md-3 control-label">Title</label>
-                  <div class="col-md-6">
-                    <input type="text" class="form-control" name="title" value="{{ old('title') ? old('title') : $course->title }}">
+              <!-- Title -->
+              <div class="form-group{{ $errors->has('title') ? ' has-error': '' }}">
+                <label class="col-md-3 control-label">Title</label>
+                <div class="col-md-6">
+                  <input type="text" class="form-control" name="title" value="{{ old('title') ? old('title') : $course->title }}">
 
-                    @if ($errors->has('title'))
-                      <span class="help-block"><strong>{{ $errors->first('title') }}</strong></span>
-                    @endif
-                  </div>
+                  @if ($errors->has('title'))
+                    <span class="help-block"><strong>{{ $errors->first('title') }}</strong></span>
+                  @endif
                 </div>
+              </div>
 
-                <!-- Course -->
-                <div class="form-group{{ $errors->has('course') ? ' has-error': '' }}">
-                  <label class="col-md-3 control-label">Course</label>
-                  <div class="col-md-6">
-                    <input type="text" class="form-control" name="course" value="{{ old('course') ? old('course') : $course->course }}">
+              <!-- Course -->
+              <div class="form-group{{ $errors->has('course') ? ' has-error': '' }}">
+                <label class="col-md-3 control-label">Course</label>
+                <div class="col-md-6">
+                  <input type="text" class="form-control" name="course" value="{{ old('course') ? old('course') : $course->course }}">
 
-                    @if ($errors->has('course'))
-                      <span class="help-block"><strong>{{ $errors->first('course') }}</strong></span>
-                    @endif
-                  </div>
+                  @if ($errors->has('course'))
+                    <span class="help-block"><strong>{{ $errors->first('course') }}</strong></span>
+                  @endif
                 </div>
+              </div>
 
-                <!-- Section -->
-                <div class="form-group{{ $errors->has('section') ? ' has-error': '' }}">
-                  <label class="col-md-3 control-label">Section</label>
-                  <div class="col-md-6">
-                    <input type="text" class="form-control" name="section" value="{{ old('section') ? old('section') : $course->section }}">
+              <!-- Section -->
+              <div class="form-group{{ $errors->has('section') ? ' has-error': '' }}">
+                <label class="col-md-3 control-label">Section</label>
+                <div class="col-md-6">
+                  <input type="text" class="form-control" name="section" value="{{ old('section') ? old('section') : $course->section }}">
 
-                    @if ($errors->has('section'))
-                      <span class="help-block"><strong>{{ $errors->first('section') }}</strong></span>
-                    @endif
-                  </div>
+                  @if ($errors->has('section'))
+                    <span class="help-block"><strong>{{ $errors->first('section') }}</strong></span>
+                  @endif
                 </div>
+              </div>
 
-                <!-- Edit Button -->
-                <div class="form-group">
-                  <div class="col-md-4 col-md-offset-6">
-                    <button type="submit" class="btn btn-success">Submit</button>
-                  </div>
+              <!-- Edit Button -->
+              <div class="form-group">
+                <div class="col-md-4 col-md-offset-7">
+                  <button type="submit" class="btn btn-success">Submit</button>
                 </div>
+              </div>
 
-              </form>
-            </div>
+            </form>
           </div>
         </div>
       </div>
@@ -121,20 +119,19 @@
     <div class="panel panel-success">
       <div class="panel-heading" role="tab" id="headingTwo">
         <h4 class="panel-title">
-          <a role="button" data-toggle="collapse" href="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">Add Quizzes, Assignments, Annoucements</a>
+          <a role="button" data-toggle="collapse" href="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">Add Assignments & Annoucements</a>
         </h4>
       </div>
 
-      <div id="collapseTwo" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingTwo">
+      <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
         <div class="panel-body">
           <div class="btn-group col-md-offset-9">
-            <button class="btn btn-default btn-lg dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               Select Type <span class="caret"></span>
             </button>
             <ul class="dropdown-menu">
-              <li><a href="#" id="annoucement">Annoucement</a></li>
               <li><a href="#" id="assignment">Assignment</a></li>
-              <li><a href="#" id="quiz">Quiz</a></li>
+              <li><a href="#" id="annoucement">Annoucement</a></li>
             </ul>
           </div>
           @include('form.assign-annouc')
@@ -143,3 +140,7 @@
     </div>
   @endif
 @endsection
+
+@push('scripts')
+  <script src="../js/toggle-assignment-type.js"></script>
+@endpush
