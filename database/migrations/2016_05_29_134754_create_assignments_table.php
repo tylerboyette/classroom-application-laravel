5649b65be31556bfa0ca0c6d0c0b40356f5c29e5
adriantelles <adriantellesweb@gmail.com>
2016-05-29 12:55:46 -0400

    Created a migration for the assignments table

---
+++ database/migrations/2016_05_29_134754_create_assignments_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAssignmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assignments', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('title');
+            $table->datetime('due_date');
+            $table->text('description');
+
+            $table->integer('course_id')->unsigned();
+
+            $table->timestamps();
+        });
+
+        /**
+         * Foreign Key Constraint
+         * 
+         */
+        Schema::table('assignments', function (Blueprint $table) {
+            $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('assignments');
+    }
+}
