46f081ae126cd0018ad94b9af29e8880162134d9
adriantelles <adriantellesweb@gmail.com>
2016-05-29 12:55:29 -0400

    Created a new class that represents the assignment model

---
+++ app/Models/Assignment.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Assignment extends Model
+{
+    /**
+     * Attributes that are mass assignable
+     *
+     * @var array
+     */
+    protected $fillable = [
+      'title', 'due_date', 'description'
+    ];
+
+    /**
+     * Format the title string to keep consistency by capitalizing
+     * the first character of each word in a string
+     * 
+     *
+     * @param String $value
+     * @return string
+     */
+    public function setTitleAttribute($value)
+    {
+      $value = strtolower($value);
+      $this->attributes['title'] = ucwords($value);
+    }
+
+    /**
+     * Format the due date appropriately
+     * 
+     * @param Date $value 
+     * @return Date
+     */
+    public function setDueDateAttribute($value)
+    {
+      $this->attributes['due_date'] = $value;
+    }
+
+    /**
+     * Apply formatting to the description attribute to keep
+     * consistency when saved to the database
+     * 
+     * @param String $value 
+     * @return string
+     */
+    public function setDescriptionAttribute($value)
+    {
+      $this->attributes['description'] = ucfirst($value);
+    }
+
+    public function course()
+    {
+        return $this->belongsTo('App\Models\Course');
+    }
+}
