1821afcd9cadef06940708bfe1163701a1c11e50
adriantelles <adriantellesweb@gmail.com>
2016-05-29 16:55:10 -0400

    Updated the index function which now passes information about recent activies in the courses that the specific user is taking

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Requests;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Auth;
+
 class HomeController extends Controller
 {
     /**
@@ -24,6 +26,23 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('pages.home');
+        $recent = array();
+
+        // Recent Activity For Current User
+        if (Auth::check()) {
+            if (count(Auth::user()->courses()->get()) > 0) {
+                foreach (Auth::user()->courses()->get() as $course) {
+                    $name = $course->subject . ' ' . $course->course . '-' . $course->section;
+                    foreach ($course->assignments()->orderBy('created_at', 'desc')->get() as $assignment) {
+                        $assignment->course_title = $name;
+                        array_push($recent, $assignment);
+                    }
+                }
+            }
+        }
+
+        return view('pages.home', [
+            'recent_assignments' => $recent
+        ]);
     }
 }
