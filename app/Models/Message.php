5c13e3fb35c81218728b264f62ef0038aec2d777
adriantelles <adriantellesweb@gmail.com>
2016-06-01 13:49:32 -0400

    Created a new model and migration for message objects

---
+++ app/Models/Message.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    /**
+     * Attributes that are mass assignable
+     * 
+     * @var array
+     */
+    protected $fillable = [
+      'title', 'message', 'from', 'to'
+    ];
+
+    /**
+     * Format the title to keep consistency by
+     * capitalizing the first character of every 
+     * word in the title
+     * 
+     * @param String $value
+     */
+    public function setTitleAttribute($value) 
+    {
+      $value = strtolower($value);
+      $this->attributes['title'] = ucwords($value);
+    }
+
+    /**
+     * Format the message to keep consistency by
+     * capitalizing the first character in the string
+     * 
+     * @param String $value
+     */
+    public function setMessageAttribute($value)
+    {
+      $this->attributes['message'] = ucfirst($value);
+    }
+}
---
+++ database/migrations/2016_06_01_173310_create_messages_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('title');
+            $table->text('message');
+            $table->integer('from')->unsigned();
+            $table->integer('to')->unsigned();
+
+            $table->timestamps();
+        });
+
+        // Foreign Key Constraint
+        Schema::table('messages', function (Blueprint $table) {
+            $table->foreign('from')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('to')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('messages');
+    }
+}
