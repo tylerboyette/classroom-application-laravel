45f0a97d5ac7325d4566781d006f66fd073915ce
adriantelles <adriantellesweb@gmail.com>
2016-06-01 18:21:36 +0000

    Created a migration for the pivot table that will define the many-to-many relationship between users and messages

---
+++ database/migrations/2016_06_01_181954_create_messages_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessagesUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('message_user', function (Blueprint $table) {
+            $table->integer('message_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('message_user');
+    }
+}
